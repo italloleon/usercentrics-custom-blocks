7c00debd9d4d783edf979b61128e02873c6a12c4
Itallo Leonardo <itallolaraujo@gmail.com>
2025-03-16 23:24:07 +0100

    remove duplicated schema

--- src/UsercentricsCustomBlocks.php
+++ src/UsercentricsCustomBlocks.php
@@ -2,8 +2,6 @@
 
 namespace ItalloLeonardo\UsercentricsCustomBlocks;
 
-use ItalloLeonardo\UsercentricsCustomBlocks\BlockScriptLoader;
-
 /**
  * The main plugin class.
  *
@@ -19,7 +17,6 @@ class UsercentricsCustomBlocks
     private string $plugin_path;
     private string $blocks_path;
     private string $plugin_url;
-    private BlockScriptLoader $blocks_script_loader;
 
     public const BLOCKS = [
         'faq-item',
@@ -28,12 +25,11 @@ class UsercentricsCustomBlocks
 
     private const BLOCKS_PATH = 'build/';
 
-    public function __construct(string $plugin_path, BlockScriptLoader $blocks_script_loader)
+    public function __construct(string $plugin_path)
     {
         $this->plugin_path = $plugin_path;
         $this->blocks_path = $plugin_path . self::BLOCKS_PATH;
         $this->plugin_url = plugin_dir_url(dirname(__FILE__));
-        $this->blocks_script_loader = $blocks_script_loader;
     }
 
 
@@ -47,24 +43,6 @@ class UsercentricsCustomBlocks
     {
         // Register hooks
         add_action('init', [$this, 'registerBlocks']);
-        // TODO: Uncomment when ready
-        // add_action('enqueue_block_editor_assets', [$this, 'enqueueEditorAssets']);
-        // add_action('wp_enqueue_scripts', [$this, 'enqueueFrontendAssets']);
-
-        // Initialize the BlockScriptLoader for the FAQ item block
-        $this->initBlockScriptLoader();
-    }
-
-    /**
-     * Initialize the BlockScriptLoader for specific blocks.
-     *
-     * @since    1.0.0
-     * @return   void
-     */
-    private function initBlockScriptLoader()
-    {
-        // Create a new BlockScriptLoader for the FAQ item block
-        $this->blocks_script_loader->init();
     }
 
     /**
@@ -83,27 +61,5 @@ class UsercentricsCustomBlocks
                 register_block_type($this->blocks_path . $block . '/block.json');
             }
         }
-    }
-
-    /**
-     * Enqueue editor assets.
-     *
-     * @since    1.0.0
-     * @return   void
-     */
-    public function enqueueEditorAssets()
-    {
-        // Enqueue scripts and styles for the editor
-    }
-
-    /**
-     * Enqueue frontend assets.
-     *
-     * @since    1.0.0
-     * @return   void
-     */
-    public function enqueueFrontendAssets()
-    {
-        // Enqueue scripts and styles for the frontend
     }
 }
