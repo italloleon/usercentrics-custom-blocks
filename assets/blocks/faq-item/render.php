7a9e8e80d5e8723e5de6e269964070cd9bfc2bd6
Itallo Leonardo <itallolaraujo@gmail.com>
2025-03-17 08:57:36 +0100

    add documentation and block render acessibility

--- assets/blocks/faq-item/render.php
+++ assets/blocks/faq-item/render.php
@@ -9,15 +9,32 @@ $block_wrapper_attributes = get_block_wrapper_attributes([
 ]);
 
 $title = $attributes['title'] ?? '';
+$item_id = 'faq-item-' . uniqid();
+$content_id = 'faq-content-' . uniqid();
 ?>
 <li <?php echo $block_wrapper_attributes; ?> itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
-	<details <?php echo isset($attributes['openByDefault']) && $attributes['openByDefault'] ? 'open' : ''; ?>>
-		<summary class="<?php echo $block_name; ?>__header">
+	<details 
+		<?php echo isset($attributes['openByDefault']) && $attributes['openByDefault'] ? 'open' : ''; ?>
+		class="<?php echo $block_name; ?>__details"
+	>
+		<summary 
+			class="<?php echo $block_name; ?>__header" 
+			aria-controls="<?php echo esc_attr($content_id); ?>"
+			id="<?php echo esc_attr($item_id); ?>"
+			aria-expanded="<?php echo isset($attributes['openByDefault']) && $attributes['openByDefault'] ? 'true' : 'false'; ?>"
+		>
 			<h3 itemprop="name" class="<?php echo $block_name; ?>__title">
 				<?php echo $title; ?>
 			</h3>
 		</summary>
-		<div class="<?php echo $block_name; ?>__content" itemprop="acceptedAnswer" itemscope itemtype="https://schema.org/Answer">
+		<div 
+			class="<?php echo $block_name; ?>__content" 
+			itemprop="acceptedAnswer" 
+			itemscope 
+			itemtype="https://schema.org/Answer"
+			id="<?php echo esc_attr($content_id); ?>"
+			aria-labelledby="<?php echo esc_attr($item_id); ?>"
+		>
 			<div itemprop="text">
 				<?php echo $children; ?>
 			</div>
